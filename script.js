f87e0673b139c976cf224764f28e0432f5aa551d
elzaem8 <elzaemprogram@gmail.com>
2025-01-11 13:55:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+function navigateTo(page) {
+    window.location.href = page;
+}
+alert("Welcome to Al-Raed Educational Center! The best place for private lessons for all educational levels in Alexandria. We offer you: a distinct explanation of all subjects and stages.  An elite group of the best specialized teachers.  Classrooms equipped to ensure student comfort.    Press the Next button to continue.")
